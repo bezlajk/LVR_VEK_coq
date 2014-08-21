2438931555a38030d600b2be10cb7311cdf469ad
bezlajk <bezlajk@gmail.com>
2014-08-21 16:01:55 +0200

    urejen_t dela, parmutacije pa še ne :)

--- insurtion_sort.v
+++ insurtion_sort.v
@@ -61,39 +61,63 @@ Proof.
        split; auto.
        apply Zle_bool_imp_le in H1. firstorder.
        intro.
-       (**
-       SearchPattern ((?a <= ?z)%Z).
-       simpl H.
-       apply IH1.
-       apply Z.leb_gt in H0.
-       split. 
-       apply Z.lt_le_incl in H0. auto.
-       
+       SearchAbout  [(_ <=? _)%Z].
+       apply Z.leb_gt in H1.
+       destruct H.
+       split. assumption.
+       replace (z :: vstavi a l) with (vstavi a (z :: l)). 
+       now apply IHl.
        simpl.
-       firstorder.
-       **)
-       admit.
+       case_eq (a<=?z)%Z.
+       proof.
+          intro.
+          absurd ((a<=?z)%Z = true). 
+          - SearchAbout ((?a <? ?z)%Z = true).
+
+           
+            rewrite -> not_true_iff_false.
+               now apply Z.leb_gt.
 
 
+            (**rewrite <- not_false_iff_true.
+            rewrite <- not_true_is_false.
+            SearchAbout (?b <> true).
+            apply eq_true_not_negb.
+            firstorder.
+            SearchPattern (?A = true -> negb ?A = false).
+            rewrite ((a <=? z)%Z <> true) with (negb ((a <=? z)%Z) = true).
+            SearchAbout (?A <> false).
+            apply not_false_iff_true.
+eq_true_not_negb.
+            replace ((a <=? z)%Z <> true) with (negb ((a <=? z)%Z) = true).
+            rewrite <- not_false_iff_true in H3.
+            rewrite <- not_false_iff_true.**)
 
-
-       (*SearchAbout ((?a < ?b)%Z -> (?a <= ?b)%Z).
-
-       simpl.
-
-
-       destruct (z :: l).
-       firstorder.
-       apply Z.leb_gt in H0.
-       apply Z.lt_le_incl in H0.
-       auto.
-       
-    
-     
-       SearchAbout (?a <=? ?b)%Z.*)   
-  admit.
+          - assumption.
+       end proof.
+       intro.
+       reflexivity.
 Qed.
 
+Lemma vstavi_pojavitev:
+  forall (x:Z)(l: list Z), S(pojavi x l)= pojavi x (vstavi x (insert l)).
+
+Proof.
+  induction l.
+  - simpl. case_eq (x=?x)%Z.
+    + intro. auto.
+    + intro. absurd ((x=?x)%Z = false).
+      * replace (x =? x)%Z with true.
+        apply diff_true_false.
+        replace (true = (x =? x)%Z) with ((x =? x)%Z=true). 
+        apply Z.eqb_refl.
+        admit.
+      * assumption.
+  - simpl.
+    case_eq((x=?a)%Z); intros.
+    + apply Z.eqb_eq in H.
+      replace a with x. simpl.
+Qed.
 
 Theorem permutacija:
   forall (l: list Z), (permutiran l (insert l)).
@@ -103,7 +127,10 @@ Proof.
   - firstorder.
   - intro.
     simpl.
-    case (x =? a)%Z.
+    case_eq (x =? a)%Z.
+    + intro.
+      apply Z.eqb_eq in H.
+      replace a with x.
     
     admit.
 admit. 

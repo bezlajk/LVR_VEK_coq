c842d5193eae0676e08b1287973e6f3610501591
bezlajk <bezlajk@gmail.com>
2014-08-26 17:57:38 +0200

    Koncali smo in smo izredno srecni. :dart:
    -------KONEC!!---------

--- insurtion_sort.v
+++ insurtion_sort.v
@@ -41,14 +41,6 @@ Proof.
         apply Zle_bool_imp_le in H0; auto.
         firstorder.
         apply Zle_bool_imp_le in H1; auto.
-   (**+ intro. simpl.
-     destruct l; simpl; firstorder.
-     * apply Z.leb_gt in H0.
-       apply Z.lt_le_incl in H0.
-       auto.
-     * apply Z.leb_gt in H0.
-       case_eq**)
-
    + firstorder; simpl.
      destruct l. 
      * firstorder.
@@ -72,52 +64,147 @@ Proof.
        proof.
           intro.
           absurd ((a<=?z)%Z = true). 
-          - SearchAbout ((?a <? ?z)%Z = true).
-
-           
-            rewrite -> not_true_iff_false.
+          - rewrite -> not_true_iff_false.
                now apply Z.leb_gt.
-
-
-            (**rewrite <- not_false_iff_true.
-            rewrite <- not_true_is_false.
-            SearchAbout (?b <> true).
-            apply eq_true_not_negb.
-            firstorder.
-            SearchPattern (?A = true -> negb ?A = false).
-            rewrite ((a <=? z)%Z <> true) with (negb ((a <=? z)%Z) = true).
-            SearchAbout (?A <> false).
-            apply not_false_iff_true.
-eq_true_not_negb.
-            replace ((a <=? z)%Z <> true) with (negb ((a <=? z)%Z) = true).
-            rewrite <- not_false_iff_true in H3.
-            rewrite <- not_false_iff_true.**)
-
           - assumption.
        end proof.
        intro.
        reflexivity.
 Qed.
 
+Lemma dodaj_glavo:
+  forall (x:Z)(l:list Z), pojavi x (x :: l) = S (pojavi x l).
+
+Proof.
+  intros x l.
+  simpl.
+  case_eq (x=?x)%Z.
+    - intro. auto.
+    - intro. absurd ((x=?x)%Z = false).
+      + SearchAbout (?A <> false).
+        rewrite -> not_false_iff_true.
+        apply Z.eqb_eq.
+        reflexivity.
+      + assumption.
+Qed.
+
+Lemma druga_glava:
+   forall (x:Z)(a:Z)(l:list Z), 
+          ((x =? a)%Z = false) -> pojavi x l = pojavi x (a :: l).
+
+Proof.
+  intros x a l H.
+  simpl.
+  case_eq (x=?a)%Z; intro.
+  - absurd ((x =? a)%Z = false).
+    + rewrite -> not_false_iff_true; assumption.
+    + assumption.
+  - reflexivity.
+Qed.
+
+Lemma ista_glava:
+   forall (x:Z)(l:list Z)(l':list Z),(pojavi x l)=(pojavi x l') -> pojavi x (x :: l) = pojavi x (x :: l').
+
+Proof.
+  intros x l l' H.
+  simpl.
+  case_eq (x =? x)%Z.
+  - intro.
+    apply eq_S.
+    assumption.
+  - intro.
+    assumption.
+Qed.
+
 Lemma vstavi_pojavitev:
-  forall (x:Z)(l: list Z), S(pojavi x l)= pojavi x (vstavi x (insert l)).
+  forall (x:Z)(l: list Z), S(pojavi x l)= pojavi x (vstavi x l).
 
 Proof.
   induction l.
   - simpl. case_eq (x=?x)%Z.
     + intro. auto.
     + intro. absurd ((x=?x)%Z = false).
-      * replace (x =? x)%Z with true.
-        apply diff_true_false.
-        replace (true = (x =? x)%Z) with ((x =? x)%Z=true). 
-        apply Z.eqb_refl.
-        admit.
+      * SearchAbout (?A <> false).
+        rewrite -> not_false_iff_true.
+        apply Z.eqb_eq.
+        reflexivity.
       * assumption.
   - simpl.
-    case_eq((x=?a)%Z); intros.
-    + apply Z.eqb_eq in H.
-      replace a with x. simpl.
+    case_eq (x =? a)%Z.
+    + intro.
+      apply Z.eqb_eq in H.
+      rewrite <- H. 
+      case_eq (x <=? x)%Z.
+      * intro.
+        rewrite -> IHl.
+        rewrite -> dodaj_glavo.
+        apply eq_S.
+        rewrite <- IHl. 
+        rewrite <- dodaj_glavo.
+        reflexivity.
+      * intro.
+        absurd ((x <=? x)%Z = false).
+        apply not_false_iff_true.
+        apply Z.leb_refl.
+        assumption.
+   + intro.
+     case_eq (x <=? a)%Z. 
+     * intro.
+       rewrite <- dodaj_glavo.
+       apply ista_glava.
+       apply druga_glava.
+       assumption.
+     * intro.
+       rewrite -> IHl.
+       apply druga_glava.
+       assumption.
 Qed.
+
+Lemma vstavi_nepojavitev:
+  forall (x:Z)(a:Z)(l: list Z), (x<>a) -> pojavi x l= pojavi x (vstavi a l).
+ 
+Proof.
+  intros x a l H.
+  induction l.
+  - simpl.
+    case_eq (x =? a)%Z.
+    + intro.
+      rewrite -> Z.eqb_eq in H0.
+      absurd (x=a); auto. 
+    + auto.
+  - simpl.
+    case_eq (x =? a)%Z.
+    + intros.
+      absurd (x=a)%Z.
+      * auto.
+      * apply Z.eqb_eq in H0; assumption.
+    + intros.
+      case_eq (x =? a0)%Z.
+      * intros.
+        case_eq (a <=? a0)%Z.
+          intros.
+          rewrite <- druga_glava ; [idtac|assumption].
+          apply Z.eqb_eq in H1.
+          replace  a0 with x. 
+          rewrite <- dodaj_glavo; auto.
+        
+          intros.
+          apply Z.eqb_eq in H1.
+          replace  a0 with x.
+          rewrite -> dodaj_glavo. 
+          apply eq_S.
+          assumption.
+      * intros.
+        case_eq (a <=? a0)%Z.
+        intros. 
+        rewrite <- druga_glava; [idtac | assumption].
+        rewrite <- druga_glava; [auto | assumption].
+    
+        intros.
+        rewrite <- druga_glava; [idtac | assumption].
+        apply IHl.     
+Qed.
+    
 
 Theorem permutacija:
   forall (l: list Z), (permutiran l (insert l)).
@@ -131,9 +218,16 @@ Proof.
     + intro.
       apply Z.eqb_eq in H.
       replace a with x.
-    
-    admit.
-admit. 
+      replace (pojavi x l) with (pojavi x (insert l)). 
+      apply vstavi_pojavitev.
+      apply permutiran_sym.
+      assumption.
+    + intro.  
+      apply Z.eqb_neq in H.
+      replace (pojavi x l) with (pojavi x (insert l)). 
+      apply vstavi_nepojavitev; assumption.
+      apply permutiran_sym.
+      assumption.
 Qed.
 
 Theorem urejenost:
@@ -145,6 +239,59 @@ Proof.
   apply urejen_t.
   assumption.
 Qed.
+
+Theorem vse_dela_pravilno:
+  forall (l : list Z), urejen (insert l) /\ permutiran l (insert l).
+
+Proof.
+  split; [apply urejenost | apply permutacija].
+Qed.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Theorem Mi_smo_najboljsa_skupina:
+      true=true.
+Admitted.
+(**"Malo zabave za profesorja. :D" **).
+
+
+
+
+
 
 
 
